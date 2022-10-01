75391fc6472034facfb068bd90949831adbdbe94
Shaiz Akhtar Mohammad <mohammad.sh@northeastern.edu>
2022-10-01 15:34:44 +0530

    First Commit

---
+++ Assignment2/src/Assignment2/Profile.java
@@ -0,0 +1,115 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Assignment2;
+
+/**
+ *
+ * @author Shaiz Akhtar
+ */
+public class Profile {
+    private String Name;
+    private int Empid;
+    private int Age;
+    private int Gender;
+    private String StartDate;
+    private String Level;
+    private String TeamInfo;
+    private String PositionTitle;
+    private int PhoneNumer;
+    private String Email;
+    private String photo;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public int getEmpid() {
+        return Empid;
+    }
+
+    public void setEmpid(int Empid) {
+        this.Empid = Empid;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int Age) {
+        this.Age = Age;
+    }
+
+    public int getGender() {
+        return Gender;
+    }
+
+    public void setGender(int Gender) {
+        this.Gender = Gender;
+    }
+
+    public String getStartDate() {
+        return StartDate;
+    }
+
+    public void setStartDate(String StartDate) {
+        this.StartDate = StartDate;
+    }
+
+    public String getLevel() {
+        return Level;
+    }
+
+    public void setLevel(String Level) {
+        this.Level = Level;
+    }
+
+    public String getTeamInfo() {
+        return TeamInfo;
+    }
+
+    public void setTeamInfo(String TeamInfo) {
+        this.TeamInfo = TeamInfo;
+    }
+
+    public String getPositionTitle() {
+        return PositionTitle;
+    }
+
+    public void setPositionTitle(String PositionTitle) {
+        this.PositionTitle = PositionTitle;
+    }
+
+    public int getPhoneNumer() {
+        return PhoneNumer;
+    }
+
+    public void setPhoneNumer(int PhoneNumer) {
+        this.PhoneNumer = PhoneNumer;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+    
+    
+    
+    
+}
---
+++ Assignment2/src/Assignment2/ProfileHist.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Assignment2;
+import java.util.*;
+
+/**
+ *
+ * @author Shaiz Akhtar
+ */
+public class ProfileHist {
+    private ArrayList<Profile>history;
+
+    public ProfileHist() {
+    this.history = new ArrayList<Profile>();
+    
+    }
+
+    public ArrayList<Profile> getHistory() {
+        return history;
+    }
+
+    public void setHistory(ArrayList<Profile> history) {
+        this.history = history;
+    }
+    
+    }
+
+    
+
+   
+    
+    
+    
+
